121b60a6b2a250f9f0b161d6091fef7a3af29325
Eric Tendian <erictendian@gmail.com>
2016-02-19 22:26:03 -0600

    Adding simple scrollspy on menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-console.log('It works!');
-
 $('html').removeClass('no-js');
+
+$(window).on('scroll', function() {
+    $('section').each(function() {
+        if ($(window).scrollTop() > $(this).offset().top+350) { // 350 is our header/intro offset
+            $('nav a').removeClass('active');
+            $('nav a[href="#'+ $(this).attr('id') +'"]').addClass('active');
+        }
+    });
+});
