92239a7f55438e1bf44a15fba962eb1c804ed905
Eric Tendian <erictendian@gmail.com>
2016-02-19 22:49:45 -0600

    Cleaning up code formatting

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $('html').removeClass('no-js');
 
 $(window).on('scroll', function() {
-    $('section').each(function() {
-        if ($(window).scrollTop() > $(this).offset().top+350) { // 350 is our header/intro offset
-            $('nav a').removeClass('active');
-            $('nav a[href="#'+ $(this).attr('id') +'"]').addClass('active');
-        }
-    });
+  $('section').each(function() {
+    if ($(window).scrollTop() > $(this).offset().top+350) { // 350 is our header/intro offset
+      $('nav a').removeClass('active');
+      $('nav a[href="#'+ $(this).attr('id') +'"]').addClass('active');
+    }
+  });
 });
